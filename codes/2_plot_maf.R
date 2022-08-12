0d7d774b6c1c7ac52e2c5589ec55602ee15636e3
Fadel DIOP <mdiop@mrc.gm>
2022-08-12 13:03:59 +0000

    Update plot_maf script

--- codes/2_plot_maf.R
+++ codes/2_plot_maf.R
@@ -1,61 +1,65 @@
-
-rm(list = ls())
-# generate_maf_plot.R
+#=================================================
+# 2_plot_maf.R
 #
 # Author: Mouhamadou F. DIOP
-# Date: 2021-11-20
+# Date: 2022-07-10
 #
 # Purpose:
 # Produce Minor Allele Frequency for each species.
 #
-# ------------------------------------------------------------------
-arguments <- commandArgs(trailingOnly = TRUE)
+#=================================================
+rm(list = ls())
 
-if(length(arguments) == 0){
-  cat("=============================================\n")
-  cat("SCRIPT SHOULD BE RUN LIKE: \n")
-  cat("Rscript 2_plot_maf.R gene_maf.tsv PerSpecies_maf.tsv")
-  cat("\n")
-  stop("NOT ENOUGH ARGUMENTS SUPPLIED .....")
-  cat("=============================================\n")
+args <- commandArgs(trailingOnly = TRUE)
+
+if(length(args) == 0){
+    print("===================================================", quote = FALSE)
+    print("        Plot Minor Allele Frequency            ", quote = FALSE)
+    print("===================================================", quote = FALSE)
+    print("", quote = FALSE)
+    print(" Usage: Rscript 2_plot_maf.R maf.tsv perSpecies_maf.tsv", quote = FALSE)
+    stop(" Not enough arguments supplied ...")
+    
 }
 
 # NOTE - uncomment these lines to install packages as needed
-devtools::install_github("thomasp85/patchwork")
+# devtools::install_github("thomasp85/patchwork")
+# install.packages("extrafont")
 
 # Load packages
 library(tidyverse)
-library(grid)
+library(extrafont)
 library(patchwork)
 
-# all_maf <- read_tsv("../results/tables/HPX15_MAF.tsv")
-all_maf <- arguments[1]
+all_maf <- args[1]
 
-genename <- gsub("_MAF.tsv", "", basename(all_maf))
+genename <- basename(dirname(all_maf))
 all_maf <- read_tsv(all_maf)
 
 ## CREATE HISTOGRAM FOR EACH SPECIES
-barfill <- "#4271AE" # "#56B4E9" "gold1"
+barfill <- "#4271AE" 
 barlines <- "#1F3552"
 
+ggtheme <- theme(axis.line = element_line(size = 1, colour = "black"),
+                 panel.grid.major = element_line(colour = "#d3d3d3"),
+                 panel.grid.minor = element_blank(),
+                 panel.border = element_blank(),
+                 panel.background = element_blank(),
+                 plot.title = element_text(size = 12, face = "bold", hjust = 0.5),
+                 axis.text.x=element_text(colour="black", size = 8),
+                 axis.text.y=element_text(colour="black", size = 8),
+                 axis.title = element_text(size = 12, face = "bold"))
+
 All <- ggplot(all_maf, aes(x = maf)) +
-  geom_histogram(aes(y = ..count..), colour = barlines, fill = "#666666") +
-  scale_x_continuous(name = "Minor Allele Frequency") +
-  scale_y_continuous(name = "Frequency") +
-  ggtitle("All") +
-  theme_bw() +
-  theme(axis.line = element_line(size=1, colour = "black"),
-        panel.grid.major = element_line(colour = "#d3d3d3"),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(), panel.background = element_blank(),
-        plot.title = element_text(size = 12, family = "Tahoma", face = "bold", hjust = 0.5),
-        text=element_text(family="Tahoma"),
-        axis.text.x=element_text(colour="black", size = 7),
-        axis.text.y=element_text(colour="black", size = 7))
+    geom_histogram(aes(y = ..count..), colour = barlines, fill = "#666666") +
+    scale_x_continuous(name = "Minor Allele Frequency") +
+    scale_y_continuous(name = "Frequency") +
+    ggtitle("All") +
+    theme_bw() +
+    ggtheme
 
 ## Load maf file
-# maf <- read_tsv("../results/tables/HPX15_PerSpecies_MAF.tsv")
-maf <- read_tsv(arguments[2])
+maf <- read_tsv(args[2])
 
 ## EXTRACT MAF FOR EACH SPECIES
 arabiensis <- maf %>% filter(Species == "Arabiensis")
@@ -65,75 +69,47 @@ sm <- maf %>% filter(Species == "S_M")
 
 
 p1 <- ggplot(arabiensis, aes(x = maf)) +
-  geom_histogram(aes(y = ..count..), colour = barlines, fill = "#00FF00") +
-  scale_x_continuous(name = "") +
-  scale_y_continuous(name = "Frequency") +
-  ggtitle("Arabiensis") +
-  theme_bw() +
-  theme(axis.line = element_line(size=1, colour = "black"),
-        panel.grid.major = element_line(colour = "#d3d3d3"),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(), panel.background = element_blank(),
-        plot.title = element_text(size = 9, family = "Tahoma", face = "bold", hjust = 0.5),
-        text = element_text(family="Tahoma"),
-        axis.text.x = element_text(colour="black", size = 5),
-        axis.text.y = element_text(colour="black", size = 5),
-        axis.title = element_text(size = 7))
+    geom_histogram(aes(y = ..count..), colour = barlines, fill = "#00FF00") +
+    scale_x_continuous(name = "") +
+    scale_y_continuous(name = "Frequency") +
+    ggtitle("Arabiensis") +
+    theme_bw() +
+    ggtheme
 
 p2 <- ggplot(coluzzi, aes(x = maf)) +
-  geom_histogram(aes(y = ..count..), colour = barlines, fill = "#0000FF") +
-  scale_x_continuous(name = "") +
-  scale_y_continuous(name = "") +
-  ggtitle("Coluzzi") +
-  theme_bw() +
-  theme(axis.line = element_line(size=1, colour = "black"),
-        panel.grid.major = element_line(colour = "#d3d3d3"),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(), panel.background = element_blank(),
-        plot.title = element_text(size = 9, family = "Tahoma", face = "bold", hjust = 0.5),
-        text = element_text(family="Tahoma"),
-        axis.text.x = element_text(colour="black", size = 5),
-        axis.text.y = element_text(colour="black", size = 5),
-        axis.title = element_text(size = 7))
+    geom_histogram(aes(y = ..count..), colour = barlines, fill = "#0000FF") +
+    scale_x_continuous(name = "") +
+    scale_y_continuous(name = "") +
+    ggtitle("Coluzzi") +
+    theme_bw() +
+    ggtheme
 
 p3 <- ggplot(gambiae, aes(x = maf)) +
-  geom_histogram(aes(y = ..count..), colour = barlines, fill = "#FF0000") +
-  scale_x_continuous(name = "Minor Allele Frequency") +
-  scale_y_continuous(name = "Frequency") +
-  ggtitle("Gambiae") +
-  theme_bw() +
-  theme(axis.line = element_line(size=1, colour = "black"),
-        panel.grid.major = element_line(colour = "#d3d3d3"),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(), panel.background = element_blank(),
-        plot.title = element_text(size = 9, family = "Tahoma", face = "bold", hjust = 0.5),
-        text = element_text(family="Tahoma"),
-        axis.text.x = element_text(colour="black", size = 5),
-        axis.text.y = element_text(colour="black", size = 5),
-        axis.title = element_text(size = 7))
+    geom_histogram(aes(y = ..count..), colour = barlines, fill = "#FF0000") +
+    scale_x_continuous(name = "Minor Allele Frequency") +
+    scale_y_continuous(name = "Frequency") +
+    ggtitle("Gambiae") +
+    theme_bw() +
+    ggtheme
 
 
 p4 <- ggplot(sm, aes(x = maf)) +
-  geom_histogram(aes(y = ..count..), colour = barlines, fill = "#4271AE") +
-  scale_x_continuous(name = "Minor Allele Frequency") +
-  scale_y_continuous(name = "") +
-  ggtitle("S_M") +
-  theme_bw() +
-  theme(axis.line = element_line(size=1, colour = "black"),
-        panel.grid.major = element_line(colour = "#d3d3d3"),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(), panel.background = element_blank(),
-        plot.title = element_text(size = 9, family = "Tahoma", face = "bold", hjust = 0.5),
-        text = element_text(family="Tahoma"),
-        axis.text.x = element_text(colour="black", size = 5),
-        axis.text.y = element_text(colour="black", size = 5),
-        axis.title = element_text(size = 7))
+    geom_histogram(aes(y = ..count..), colour = barlines, fill = "#4271AE") +
+    scale_x_continuous(name = "Minor Allele Frequency") +
+    scale_y_continuous(name = "") +
+    ggtitle("S_M") +
+    theme_bw() +
+    ggtheme
 
 
 All + wrap_plots(p1, p2, p3, p4)
 
 # save to file
-ggsave(paste0("../results/figures/", genename, "_maf.png"))
+output <- paste0("results/figures/", genename)
+if(!dir.exists(output)) dir.create(output)
+
+ggsave(paste0(output, "/maf.pdf"),
+       width = 200, height = 150, units = "mm", dpi = 600)
 
 # #========================
 # ## GROUPED BARCHART PLOT
@@ -159,8 +135,8 @@ ggsave(paste0("../results/figures/", genename, "_maf.png"))
 #         panel.grid.major = element_line(colour = "#d3d3d3"),
 #         panel.grid.minor = element_blank(),
 #         panel.border = element_blank(), panel.background = element_blank(),
-#         # plot.title = element_text(size = 9, family = "Tahoma", face = "bold", hjust = 0.5),
-#         text = element_text(family="Tahoma", face = "bold"),
+#         # plot.title = element_text(size = 9, family = "Comic Sans MS", face = "bold", hjust = 0.5),
+#         text = element_text(family="Comic Sans MS", face = "bold"),
 #         axis.text.x = element_text(colour="black", size = 5),
 #         axis.text.y = element_text(colour="black", size = 5),
 #         axis.title = element_text(size = 7))
